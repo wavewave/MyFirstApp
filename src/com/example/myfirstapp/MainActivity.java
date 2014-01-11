0159609fd7ce3a7713f543ef063943a7390fba80
Ian-Woo Kim <ianwookim@gmail.com>
2014-01-10 17:40:34 -0800

    first drawing app

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.myfirstapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent; 
+
 
 public class MainActivity extends Activity
 {
+    SingletouchView mSingletouchView; 
+
+    private static final String TAG = "MainActivity";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        // mSingletouchView = new SingletouchView(,);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // setContentView(mSingletouchView);
     }
+    /** no back button */
+    @Override
+    public void onBackPressed() { 
+        Log.i(TAG, "back"); 
+    }
+
 }
---
+++ src/com/example/myfirstapp/SingletouchView.java
@@ -0,0 +1,58 @@
+package com.example.myfirstapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class SingletouchView extends View {
+    private Paint paint = new Paint();
+    private Path path = new Path();
+    
+    private static final String TAG = "SingletouchView"; 
+    public SingletouchView(Context context, AttributeSet attrs) {
+	super(context, attrs);
+        Log.i(TAG,"startview");
+	paint.setAntiAlias(true);
+	paint.setStrokeWidth(6f);
+	paint.setColor(Color.WHITE);
+	paint.setStyle(Paint.Style.STROKE);
+	paint.setStrokeJoin(Paint.Join.ROUND); 
+    }
+
+  
+  @Override
+  protected void onDraw(Canvas canvas) {
+      canvas.drawPath(path, paint);
+  }
+  
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+      float eventX = event.getX();
+      float eventY = event.getY();
+
+      switch (event.getAction()) {
+      case MotionEvent.ACTION_DOWN:
+	  path.moveTo(eventX, eventY);
+	  return true;
+      case MotionEvent.ACTION_MOVE:
+	  path.lineTo(eventX, eventY);
+	  break;
+      case MotionEvent.ACTION_UP:
+	  // nothing to do
+	  break;
+      default:
+	  return false;
+      }
+
+      // Schedules a repaint.
+      invalidate();
+      return true;
+      } 
+} 
