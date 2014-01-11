dce73cfa444a5b45f8a5e9405a7ba8fd8b0ce3e8
Ian-Woo Kim <ianwookim@gmail.com>
2014-01-11 15:01:02 -0800

    success in get historical values (understand motion event)

--- src/com/example/myfirstapp/SingletouchView.java
+++ src/com/example/myfirstapp/SingletouchView.java
@@ -14,12 +14,52 @@ public class SingletouchView extends View {
     private Paint paint = new Paint();
     private Path path = new Path();
     
+    private OnHoverListener ohl = new OnHoverListener() { 
+	     @Override 
+              public boolean onHover( View v, MotionEvent ev ) {
+		 // Log.i( TAG, ev.toString() ); 
+		 printSamples ( ev) ;
+                  switch (ev.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+			// path.moveTo(eventX, eventY);
+			Log.i(TAG,"hover down");
+	                return true;
+                    case MotionEvent.ACTION_MOVE:
+                        //Log.i(TAG,"hover move");
+                	return true;
+                    case MotionEvent.ACTION_UP:
+                	  // nothing to do
+		        Log.i(TAG,"hover up"); 
+	                return true;
+		  }
+
+		  return true;                                          
+              }
+	};
+
+  
+    void printSamples(MotionEvent ev) {
+	final int historySize = ev.getHistorySize();
+	final int pointerCount = ev.getPointerCount();
+	for (int h = 0; h < historySize; h++) {
+	    Log.i(TAG, "At time " +  String.valueOf (ev.getHistoricalEventTime(h)));
+	    for (int p = 0; p < pointerCount; p++) {
+		Log.i(TAG, "  pointer " + String.valueOf (ev.getHistoricalX(p,h)) + " "  + String.valueOf (ev.getHistoricalY(p,h)));
+	    }
+	}  
+	Log.i(TAG,"At time " + String.valueOf (ev.getEventTime()));
+	for (int p = 0; p < pointerCount; p++) {
+	    Log.i(TAG, "  pointer " + String.valueOf (ev.getX(p)) + String.valueOf (ev.getY(p)));
+	}
+    }
+
     private static final String TAG = "SingletouchView"; 
     public SingletouchView(Context context, AttributeSet attrs) {
 	super(context, attrs);
         Log.i(TAG,"startview");
+        this.setOnHoverListener( ohl );
 	paint.setAntiAlias(true);
-	paint.setStrokeWidth(6f);
+	paint.setStrokeWidth(1f);
 	paint.setColor(Color.WHITE);
 	paint.setStyle(Paint.Style.STROKE);
 	paint.setStrokeJoin(Paint.Join.ROUND); 
@@ -28,21 +68,30 @@ public class SingletouchView extends View {
   
   @Override
   protected void onDraw(Canvas canvas) {
-      canvas.drawPath(path, paint);
+      // canvas.drawPath(path, paint);
   }
   
-
+  @Override
+  public boolean onGenericMotionEvent( MotionEvent event ) {
+      Log.i(TAG,event.toString());
+      return true;
+  }
   @Override
   public boolean onTouchEvent(MotionEvent event) {
+      Log.i(TAG, event.toString()) ;
       float eventX = event.getX();
       float eventY = event.getY();
+ 
 
       switch (event.getAction()) {
       case MotionEvent.ACTION_DOWN:
-	  path.moveTo(eventX, eventY);
+         
+	  // path.moveTo(eventX, eventY);
 	  return true;
       case MotionEvent.ACTION_MOVE:
-	  path.lineTo(eventX, eventY);
+          //Log.i(TAG, event.toString());
+
+	  // path.lineTo(eventX, eventY);
 	  break;
       case MotionEvent.ACTION_UP:
 	  // nothing to do
@@ -55,4 +104,5 @@ public class SingletouchView extends View {
       invalidate();
       return true;
       } 
-} 
+
+}
